22484ef9766746080812cb7c0298c9610d4a9f07
Matt <mattdanielbarnett@gmail.com>
2022-04-13 15:03:34 +0100

    Added initial name field and button to add-type form within the add-type page

--- lib/pages/typeadd.dart
+++ lib/pages/typeadd.dart
@@ -11,23 +11,82 @@ class HistoryTypeAdd extends StatefulWidget {
 }
 
 class _HistoryTypeAddState extends State<HistoryTypeAdd> {
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: HistColours.cBack,
       appBar: secondaryAppbar(context),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Center(
-              child: Text(
-                "Placeholder - Add Type",
-                style: TextStyle(
-                    fontSize: 32
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:  [
+         Container(
+      padding: const EdgeInsets.all(20.0),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: HistColours.cForward,
+                    fontWeight: FontWeight.w900,
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the type name.';
+                    } else {
+                      //name = value;
+                    }
+                    return null;
+                  },
+                  cursorColor: HistColours.cHighlight,
+                  decoration: const InputDecoration(
+                    focusColor: HistColours.cHighlight,
+                    labelText: 'Type Name',
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w900,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: HistColours.cHighlight),
+                    )
+                  ),
                 ),
-              )
-          ),
+                Container(
+                  width: screenWidth,
+                  child: ElevatedButton(
+                    child: const Text(
+                      'Submit',
+                      style: TextStyle(
+                        color: HistColours.cBack,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 18,
+                      ),
+                    ),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Adding record type...'),
+                          ),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: HistColours.cHighlight
+                    )
+                  ),
+                  margin: const EdgeInsets.only(top: 20.0),
+                )
+              ],
+            ))),
         ],
       ),
     );
