7b3272584fcb1f3f11bd963a30e4c3b1e704b5b0
Matt <mattdanielbarnett@gmail.com>
2022-04-24 23:30:41 +0100

    Added settings ui package and example settings tiles on the settings page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import '../shared/colours.dart';
 import 'package:history_logging_app/template/secondary_appbar.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class HistorySettings extends StatefulWidget {
   const HistorySettings ({Key? key}) : super(key: key);
@@ -16,20 +17,38 @@ class _HistorySettingsState extends State<HistorySettings> {
     return Scaffold(
       backgroundColor: HistColours.cBack,
       appBar: secondaryAppbar(context),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Center(
-              child: Text(
-                "Placeholder - Settings",
-                style: TextStyle(
-                    fontSize: 32
-                ),
-              )
+      body: SettingsList(
+        darkTheme: const SettingsThemeData(
+          settingsListBackground: Colors.transparent,
+        ),
+        lightTheme: const SettingsThemeData(
+          settingsListBackground: Colors.transparent,
+        ),
+        sections: [
+          SettingsSection(
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.home, color: HistColours.cHighlight),
+                title: const Text('Example 1'),
+                value: const Text('Example 1'),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.navigation(
+                leading: const Icon(Icons.home, color: HistColours.cHighlight),
+                title: const Text('Example 2'),
+                value: const Text('Example 2'),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.navigation(
+                leading: const Icon(Icons.home, color: HistColours.cHighlight),
+                title: const Text('Example 3'),
+                value: const Text('Example 3'),
+                onPressed: (BuildContext context) {},
+              ),
+            ],
           ),
-        ],
-      ),
+        ]
+      )
     );
   }
 }
