5553dee256bcdac2ff561929f5cf2b12c83f9551
Matt <mattdanielbarnett@gmail.com>
2022-04-25 17:17:14 +0100

    Added sliding capability to record types within the record types menu

--- lib/shared/histglobals.dart
+++ lib/shared/histglobals.dart
@@ -80,6 +80,7 @@ List<Widget> getRecordWidgets(BuildContext context) {
   return recordsWidgets;
 }
 
+// This type container is for the "Add Records" page
 Widget tAddContainer(BuildContext context, RecordType currentType) {
   double screenWidth = MediaQuery.of(context).size.width;
 
@@ -91,81 +92,70 @@ Widget tAddContainer(BuildContext context, RecordType currentType) {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 15),
-          Row(
-            children: [
-              RichText(
-                text: TextSpan(
-                  children: <TextSpan>[
-                    const TextSpan(
-                      text: 'Type: ',
-                      style: TextStyle(
-                        color: HistColours.cBack,
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 18,
-                      ),
-                    ),
-                    TextSpan(
-                      text: currentType.getTitle(),
-                      style: const TextStyle(
-                        color: HistColours.cBack,
-                        fontWeight: FontWeight.w300,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
+          RichText(
+            text: TextSpan(
+              children: <TextSpan>[
+                const TextSpan(
+                  text: 'Type: ',
+                  style: TextStyle(
+                    color: HistColours.cBack,
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 5),
-          Row(
-            children: [
-              Flexible(
-                child: Text(
-                  currentType.getAuthor() + ", " + currentType.getCreationDT(),
+                TextSpan(
+                  text: currentType.getTitle(),
                   style: const TextStyle(
                     color: HistColours.cBack,
-                    fontWeight: FontWeight.w900,
-                    fontSize: 12,
-                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
           const SizedBox(height: 5),
-          Row(
-            children: [
-              Flexible(
-                child: RichText(
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      const TextSpan(
-                        text: 'Description: ',
-                        style: TextStyle(
-                          color: HistColours.cBack,
-                          fontWeight: FontWeight.w500,
-                          fontStyle: FontStyle.italic,
-                          fontSize: 12,
-                        ),
-                      ),
-                      TextSpan(
-                        text: currentType.getDesc(),
-                        style: const TextStyle(
-                          color: HistColours.cBack,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
+          Flexible(
+            child: Text(
+              currentType.getAuthor() + ", " + currentType.getCreationDT(),
+              style: const TextStyle(
+                color: HistColours.cBack,
+                fontWeight: FontWeight.w900,
+                fontSize: 12,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          Flexible(
+          child: RichText(
+            text: TextSpan(
+              children: <TextSpan>[
+                const TextSpan(
+                  text: 'Description: ',
+                  style: TextStyle(
+                    color: HistColours.cBack,
+                    fontWeight: FontWeight.w500,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 12,
                   ),
                 ),
-              ),
-            ],
+                TextSpan(
+                  text: currentType.getDesc(),
+                  style: const TextStyle(
+                    color: HistColours.cBack,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
-      ));
+        ),
+      ],
+    ),
+  );
 
   return Column(
     children: [
@@ -174,24 +164,17 @@ Widget tAddContainer(BuildContext context, RecordType currentType) {
         color: HistColours.cHighlight,
             borderRadius: BorderRadius.all(Radius.circular(20))),
         child: Slidable(
-          // Specify a key if the Slidable is dismissible.
           key: const ValueKey(0),
-
-          // The start action pane is the one at the left or the top side.
           startActionPane: ActionPane(
-            extentRatio: 0.6,
-            // A motion is a widget used to control how the pane animates.
+            extentRatio: 0.45,
             motion: const BehindMotion(),
-
-            // All actions are defined in the children parameter.
             children: [
-              // A SlidableAction can have an icon and/or a label.
               SlidableAction(
                 onPressed: (BuildContext context) {},
                 backgroundColor: Colors.transparent,
                 foregroundColor: Colors.white,
-                icon: Icons.delete,
-                label: 'Delete',
+                icon: Icons.add,
+                label: 'Add',
               ),
               SlidableAction(
                 onPressed: (BuildContext context) {},
@@ -199,13 +182,6 @@ Widget tAddContainer(BuildContext context, RecordType currentType) {
                 foregroundColor: Colors.white,
                 icon: Icons.share,
                 label: 'Share',
-              ),
-              SlidableAction(
-                onPressed: (BuildContext context) {},
-                backgroundColor: Colors.transparent,
-                foregroundColor: Colors.white,
-                icon: Icons.add,
-                label: 'Add',
               ),
             ],
           ),
@@ -237,12 +213,13 @@ Widget tAddContainer(BuildContext context, RecordType currentType) {
   );
 }
 
+// This type container is for the "Record Types" page
 Widget tTypeContainer(BuildContext context, RecordType currentType) {
   double screenWidth = MediaQuery.of(context).size.width;
 
-  return Container(
+  Container content = Container(
       height: 190,
-      width: screenWidth * 0.9,
+      width: screenWidth * 0.84,
       margin: const EdgeInsets.only(
           bottom: 20
       ),
@@ -255,110 +232,158 @@ Widget tTypeContainer(BuildContext context, RecordType currentType) {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 15),
-          Row(
-            children: [
-              const SizedBox(width: 15),
-              RichText(
-                text: TextSpan(
-                  children: <TextSpan>[
-                    const TextSpan(text: 'Type: ',
-                      style: TextStyle(
-                        color: HistColours.cBack,
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 18,
-                      ),
-                    ),
-                    TextSpan(text: currentType.getTitle(),
-                      style: const TextStyle(
-                        color: HistColours.cBack,
-                        fontWeight: FontWeight.w300,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
+          RichText(
+            text: TextSpan(
+              children: <TextSpan>[
+                const TextSpan(
+                  text: 'Type: ',
+                  style: TextStyle(
+                    color: HistColours.cBack,
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 5),
-          Row(
-            children: [
-              const SizedBox(width: 15),
-              Flexible(
-                child: Text(
-                  currentType.getAuthor() + ", " + currentType.getCreationDT(),
+                TextSpan(
+                  text: currentType.getTitle(),
                   style: const TextStyle(
                     color: HistColours.cBack,
-                    fontWeight: FontWeight.w900,
-                    fontSize: 12,
-                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
           const SizedBox(height: 5),
-          Row(
-            children: [
-              const SizedBox(width: 15),
-              Flexible(
-                child: RichText(
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      const TextSpan(text: 'Description: ',
-                        style: TextStyle(
-                          color: HistColours.cBack,
-                          fontWeight: FontWeight.w500,
-                          fontStyle: FontStyle.italic,
-                          fontSize: 12,
-                        ),
-                      ),
-                      TextSpan(text: currentType.getDesc(),
-                        style: const TextStyle(
-                          color: HistColours.cBack,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+          Flexible(
+            child: Text(
+              currentType.getAuthor() + ", " + currentType.getCreationDT(),
+              style: const TextStyle(
+                color: HistColours.cBack,
+                fontWeight: FontWeight.w900,
+                fontSize: 12,
+                fontStyle: FontStyle.italic,
               ),
-            ],
+            ),
+          ),
+          const SizedBox(height: 5),
+          Flexible(
+            child: RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  const TextSpan(
+                    text: 'Description: ',
+                    style: TextStyle(
+                      color: HistColours.cBack,
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 12,
+                    ),
+                  ),
+                  TextSpan(
+                    text: currentType.getDesc(),
+                    style: const TextStyle(
+                      color: HistColours.cBack,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           const SizedBox(height: 45),
-          Row(
-            children: [
-              const SizedBox(width: 15),
-              Flexible(
-                child: RichText(
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      const TextSpan(text: 'Format: ',
-                        style: TextStyle(
-                          color: HistColours.cBack,
-                          fontWeight: FontWeight.w500,
-                          fontStyle: FontStyle.italic,
-                          fontSize: 12,
-                        ),
-                      ),
-                      TextSpan(text: currentType.getFormat(),
-                        style: const TextStyle(
-                          color: HistColours.cBack,
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.italic,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
+          Flexible(
+            child: RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  const TextSpan(
+                    text: 'Format: ',
+                    style: TextStyle(
+                      color: HistColours.cBack,
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 12,
+                    ),
                   ),
-                ),
+                  TextSpan(
+                    text: currentType.getFormat(),
+                    style: const TextStyle(
+                      color: HistColours.cBack,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+      ],
+    ),
+  );
+
+  return Column(
+    children: [
+      Container(
+        decoration: const BoxDecoration(
+            color: HistColours.cHighlight,
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: Slidable(
+          key: const ValueKey(0),
+          startActionPane: ActionPane(
+            extentRatio: 0.6,
+            motion: const BehindMotion(),
+            children: [
+              SlidableAction(
+                onPressed: (BuildContext context) {},
+                backgroundColor: Colors.transparent,
+                foregroundColor: Colors.white,
+                icon: Icons.delete,
+                label: 'Delete',
+              ),
+              SlidableAction(
+                onPressed: (BuildContext context) {},
+                backgroundColor: Colors.transparent,
+                foregroundColor: Colors.white,
+                icon: Icons.edit,
+                label: 'Edit',
+              ),
+              SlidableAction(
+                onPressed: (BuildContext context) {},
+                backgroundColor: Colors.transparent,
+                foregroundColor: Colors.white,
+                icon: Icons.share,
+                label: 'Share',
               ),
             ],
           ),
-        ],
+          child: Row(
+            children: [
+              SizedBox(
+                width: screenWidth * 0.02,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                  color: HistColours.cBack,
+                ),
+                width: screenWidth * 0.01,
+                height: 80,
+              ),
+              SizedBox(
+                width: screenWidth * 0.03,
+              ),
+              content,
+            ],
+          ),
+        ),
+      ),
+      const SizedBox(
+        height: 20,
       )
+    ],
   );
 }
 
